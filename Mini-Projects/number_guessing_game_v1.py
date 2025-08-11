611d415a9e8f8e2c88ace8851371ece0b5a9508e
Amirofcodes <j.bouddehbine@it-students.fr>
2025-08-11 12:18:55 +0200

    feat(Mini-Projects): complete Enhanced Number Guessing Game v1

---
+++ Mini-Projects/number_guessing_game_v1.py
@@ -0,0 +1,22 @@
+import random
+
+print("******* Welcome to the number-guessing game *******")
+num = random.randint(1, 20)
+attempts = 0
+while attempts < 6:
+    guess = int(input("Enter your guess (1 to 20): "))
+    if guess <= 0 or guess > 20:
+        print("guess must be between 1 and 20")
+    elif guess < num:
+        attempts += 1
+        print("Higher")
+    elif guess > num:
+        attempts += 1
+        print("Lower")
+    else:
+        attempts += 1
+        print(
+            f"very good!!! {guess} is the correct guess. You used {attempts} attempts.")
+        break
+else:
+    print("You've reach the max Allowed number of guesses")
